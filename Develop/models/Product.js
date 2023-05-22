60f92758d136788dfb3cff08766ef41b5ac169ad
unknown <matthewfive.30@gmail.com>
2023-05-22 17:03:09 -0500

    Some formatting issues. Final

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,13 +1,9 @@
-// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
-const sequelize = require('../config/connection');
-const Category = require('./Category');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
+const sequelize = require('../config/connection');
+
 class Product extends Model {}
 
-// set up fields and rules for Product model
 Product.init(
   {
     id: {
